89c91b175f2a412870900e8a680c084ef39081ce
TonyJem <a.jemanov@gmail.com>
2021-01-16 09:46:48 +0200

    Add logic to "panAction" enable to move "blue" UIView

--- petUIGestureRecognizer/petUIGestureRecognizer/ViewController.swift
+++ petUIGestureRecognizer/petUIGestureRecognizer/ViewController.swift
@@ -1,13 +1,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    
-    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+    @IBAction func panAction(_ gesture: UIPanGestureRecognizer) {
+        
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        
+        gestureView.center = CGPoint(
+            x: gestureView.center.x + gestureTranslation.x,
+            y: gestureView.center.y + gestureTranslation.y
+        )
+        
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
     }
 }
